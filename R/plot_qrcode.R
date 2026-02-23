83abb66c116591a9a61108a489611ea56a9072a8
RichardJTelford <Richard.Telford@uib.no>
2026-02-23 09:50:12 +0100

    add url to argument instead of qrcode

--- R/plot_qrcode.R
+++ R/plot_qrcode.R
@@ -1,20 +1,22 @@
-#' Plot qrcode with passcode
-#' @param x qrcode
+#' Plot qrcode with access code
+#' @param url url to make into a QR code
 #' @param col colours for the qrcode
 #' @param main Plot title
-#' @param passcode passcode
+#' @param code_text Access code title
+#' @param accesscode accesscode
 #' @importFrom graphics par image mtext
 #' @importFrom glue glue
 #' @export
 
-plot_qrcode <- function(x, col = c("white", "black"),
-                        main = "title", passcode) {
+plot_qrcode <- function(url, col = c("white", "black"),
+                        main = "title", code_text = "Access code", accesscode) {
+  x <- qr_code(url)
   z <- t(x)[, rev(seq_len(ncol(x)))]
   old_par <- par(no.readonly = TRUE)
   on.exit(par(old_par), add = TRUE, after = FALSE)
   par(mar = rep(1.1, 4))
   image(z, asp = 1, col = col, axes = FALSE, main = main)
-  if (!missing(passcode)) {
-    mtext(glue("passcode: {passcode}"), side = 1, line = 0, cex = 1.5)
+  if (!missing(accesscode)) {
+    mtext(glue("{code_text}: {accesscode}"), side = 1, line = 0, cex = 1.5)
   }
 }
