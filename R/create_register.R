5a27962f4409cab0a805475ca9362baa0c7816c5
RichardJTelford <Richard.Telford@uib.no>
2026-02-23 09:48:49 +0100

    rename function and move to accesscode

--- R/create_register.R
+++ R/create_register.R
@@ -1,7 +1,7 @@
 #' Create attendance quiz and qr code
 #' @param course_id id number of the course.
 #' Go to your course home page and the number is at the end of the url.
-#' @param passcode the passcode
+#' @param accesscode the accesscode
 #' @param quiz_params Quiz parameters.
 #' You can make these with [collate_quiz_params()]
 
@@ -11,49 +11,41 @@
 #' @importFrom grDevices dev.new
 #' @export
 
-create_attendence <- function(
+create_register <- function(
   course_id,
-  passcode = random_passcode(adjective_noun_list = adjective_noun$norsk),
-  quiz_params = collate_quiz_params()
+  accesscode = random_passcode(adjective_noun_list = adjective_noun$norsk),
+  quiz_params = collate_quiz_params(),
+  quiz_id
 ) {
   # authenticate
   auth <- canvas_authenticate()
 
-  # make quiz
-  quiz <- create_quiz(
-    canvas = auth,
-    course_id = course_id,
-    quiz_params = quiz_params
-  )
+  # make quiz if ID not provided
+  if(missing(quiz_id)) {
+    quiz <- create_quiz(
+      canvas = auth,
+      course_id = course_id,
+      quiz_params = quiz_params
+    )
 
-  # get quiz id
-  quiz_id <- quiz$id
-
-  # make question contents with passcode
-  quiz_question_params <- collate_question_params(answers = passcode)
-
-  # create question
-  quest <- create_quiz_question(
-    canvas = auth,
-    course_id = course_id,
-    quiz_id = quiz_id,
-    quiz_question_params = quiz_question_params
-  )
+    # get quiz id
+    quiz_id <- quiz$id
+  }
 
   # publish quiz
   quiz <- activate_quiz(
     course_id = course_id,
     quiz_id = quiz_id,
-    unlock_at = now()
+    unlock_at = now(),
+    access_code = accesscode
   )
 
 
   # get url
   quiz_url <- quiz$mobile_url
-  # print qrcode + passcode
+  # print qrcode + accesscode
   dev.new()
-  qr_code(quiz_url) |>
-    plot_qrcode(main = "Please Register", passcode = passcode)
+  plot_qrcode(url = quiz_url, main = "Please Register", accesscode = accesscode)
 
-  invisible(list(quiz = quiz, question = quest))
+  invisible(list(quiz = quiz))
 }
